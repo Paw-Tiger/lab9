0632816e1349e978ca8ad834e147d44842dc88dd
Dmitriy Kalnichenko <kalnichjenko1996@gmail.com>
2019-03-14 19:45:13 +0200

    add image

---
+++ templates/image.php
@@ -0,0 +1,17 @@
+<?php
+
+var_dump($_FILES);
+?>
+
+
+<form method="post" enctype="multipart/form-data">
+    <div class="input-group">
+        <div class="custom-file">
+            <input type="file" class="custom-file-input" id="inputGroupFile04" name="image[]" multiple>
+            <label class="custom-file-label" for="inputGroupFile04">Choose file</label>
+        </div>
+        <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="submit" id="inputGroupFileAddon04">Button</button>
+        </div>
+    </div>
+</form>
